5a0c67c9c90cd8115bc0050d04bc99fd8bbf7f15
etagwerker <ernesto@etagwerker.com>
2011-10-23 17:49:02 -0300

    Initial.

--- js/application.js
+++ js/application.js
@@ -1,32 +1,59 @@
-jQuery.ready(
+$(document).ready(
 
   function() {
-
+    
     window.Provincia = Backbone.Model.extend({
 
     });
     
-    window.ProvinciasCollection = Backbone.Collection.extend({ 
+    window.Departamento = Backbone.Model.extend({
+
+    });    
+    
+    window.DepartamentosCollection = Backbone.Collection.extend({ 
+      url: 'http://censo.heroku.com/departamentos?callback=?', 
+      model: Departamento
       
+    });
+    
+    window.ProvinciasCollection = Backbone.Collection.extend({ 
+      url: 'http://censo.heroku.com/provincias?callback=?', 
       model: Provincia
       
     });
     
+    
     window.Provincias = new ProvinciasCollection();
+    window.Departamentos = new DepartamentosCollection();    
     
     window.ProvinciasView = Backbone.View.extend({
 
       el:  $("div.content"), 
+      
+      template: "<b>Pepe</b>", 
       
       render: function() {
         this.el.html("<h1>Pepe</h1>");
       }
 
     });
-    
+      
     window.App = new ProvinciasView;
     
-    
+    window.Provincias.fetch({
+              success: function() {
+                console.log('Provincias cargadas: ' + window.Provincias.length + '.');
+              }, 
+              error: function() {
+                console.log('Error cargando provincias!');
+              }});
+    window.Departamentos.fetch({
+              success: function() {
+                console.log('Departamentos Cargados: ' + window.Departamentos.length + '.');
+              }, 
+              error: function() {
+                console.log('Error cargando departamentos!');
+              }});    
   }
   
 );
