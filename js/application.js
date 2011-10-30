8cb7d83bca320096604007ff4fa4375e25a582ff
etagwerker <ernesto@etagwerker.com>
2011-10-30 13:31:41 -0300

    Agregué localidades a la UI.

--- js/application.js
+++ js/application.js
@@ -1,20 +1,20 @@
 $(document).ready(
-
+  
   function() {
+    function tidy_id(_id) { 
+      _id = _id.replace('í','i');
+      _id = _id.replace('ú','u');
+      _id = _id.replace('ó','o');
+      _id = _id.replace('é','e');
+      _id = _id.replace('á','a');
+      return _id;                                
+    }; 
     
     window.Population = Backbone.Model.extend({
-      tidy_id: function(_id) { 
-        _id = _id.replace('í','i');
-        _id = _id.replace('ú','u');
-        _id = _id.replace('ó','o');
-        _id = _id.replace('é','e');
-        _id = _id.replace('á','a');
-        return _id;                                
-      },
       
       url: function() {
         var area = this.get('area');
-        return 'http://censo.heroku.com/poblacion/' + this.tidy_id(area.id) + "/totales?callback=?";
+        return 'http://censo.heroku.com/poblacion/' + tidy_id(area.id) + "/totales?callback=?";
       }, 
       
       parse: function(response) {
@@ -24,6 +24,20 @@ $(document).ready(
     
     window.Provincia = Backbone.Model.extend({
       
+      departamentos: function() {
+        if (this.get('departamentos')) {
+          return this.get('departamentos');
+        } else {
+          var provincia = this;
+          var collection = new DepartamentosCollection();
+          collection.fetch({success: function() {
+              provincia.set({departamentos: collection});
+              window.DepartamentosPartialView.addAll();
+            }
+          });
+        }
+      }
+      
     });
     
     window.Departamento = Backbone.Model.extend({
@@ -32,13 +46,13 @@ $(document).ready(
 
     // class DepartamentosCollection
     window.DepartamentosCollection = Backbone.Collection.extend({ 
-      url: 'http://censo.heroku.com/departamentos?callback=?', 
-      model: Departamento
+      model: Departamento, 
+      
+      url: function() {
+        return 'http://censo.heroku.com/' + tidy_id(window.CurrentProvincia.get('id')) + '/departamentos?callback=?';
+      }
       
     });
-
-    // object Departamentos
-    window.Departamentos = new DepartamentosCollection();    
 
     // class ProvinciasCollection    
     window.ProvinciasCollection = Backbone.Collection.extend({ 
@@ -49,6 +63,9 @@ $(document).ready(
     
     // object Provincias        
     window.Provincias = new ProvinciasCollection();
+
+    // object Departamentos
+    window.Departamentos = new DepartamentosCollection();
     
     window.PopulationView = Backbone.View.extend({
       template: _.template($('#population-template').html()),
@@ -65,7 +82,8 @@ $(document).ready(
 
         // Create and draw the visualization.
         var chart = new google.visualization.PieChart(document.getElementById('population-graph'));
-        chart.draw(data, {width: 450, height: 300, title: 'Personas en San Nicolás'});
+        var area = population.get('area');
+        chart.draw(data, {width: 450, height: 300, title: 'Personas en ' + area.get('nombre') });
       },
       
       initialize: function() {
@@ -75,6 +93,7 @@ $(document).ready(
             v.model.set({population: p});
             $("div#population-detail").html(v.render().el);
             v.population_graph(p);
+            window.DepartamentosPartialView = new DepartamentosView({model: v.model});            
           }
         });
       }, 
@@ -82,6 +101,20 @@ $(document).ready(
       render: function() {
         var population = this.model.get('population');
         $(this.el).html(this.template(population.toJSON()));
+        return this;
+      }
+      
+    });
+    
+    window.DepartamentoView = Backbone.View.extend({
+      template: _.template($('#departamento-template').html()),
+      
+      initialize: function() {
+        
+      },
+
+      render: function() {
+        $(this.el).html(this.template(this.model.toJSON()));
         return this;
       }
       
@@ -100,6 +133,7 @@ $(document).ready(
       },
 
       select: function() {
+        window.CurrentProvincia = this.model;
         this.model.set({selected: true});
         new PopulationView({model: this.model});
       },
@@ -112,11 +146,28 @@ $(document).ready(
 
     });
     
-    window.ProvinciasView = Backbone.View.extend({      
+    window.DepartamentosView = Backbone.View.extend({      
       initialize: function() {
-        
+        this.model.departamentos();
+      },
+
+      // Add a single departamento to the list by creating a view for it, and
+      // appending its element to the `<ul>`.
+      addOne: function(departamento) {
+        var view = new DepartamentoView({model: departamento});
+        $("ul#departamentos-list").append(view.render().el);
+      },
+
+      // Add all items in the **Provincias** collection at once.
+      addAll: function() {
+        var departamentos = this.model.departamentos();
+        departamentos.each(this.addOne);
+      }
+    });
+    
+    window.ProvinciasView = Backbone.View.extend({      
+      initialize: function() {    
         this.addAll();
-        
       },
       
       // Add a single provincia to the list by creating a view for it, and
@@ -148,14 +199,6 @@ $(document).ready(
           }, 
           error: function() {
             console.log('Error cargando provincias!');
-          }
-        });
-        window.Departamentos.fetch({
-          success: function() {
-            console.log('Departamentos Cargados: ' + window.Departamentos.length + '.');
-          }, 
-          error: function() {
-            console.log('Error cargando departamentos!');
           }
         });
       }
